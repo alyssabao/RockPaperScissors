465886562eb0af3f8e849de661ce07dea926ffd5
alyssabao <64719777+alyssabao@users.noreply.github.com>
2020-05-26 17:19:13 +0700

    completed 3 rockets

--- src/App.js
+++ src/App.js
@@ -18,26 +18,32 @@ const choices = {
 };
 
 let result;
+let className;
 
 function App() {
   let [userC, setUserC] = useState({})
   let [computerC, setComputerC] = useState({})
   const [gameHistory, setGameHistory] = useState([]);
+  const [flawless, setFlawless] = useState(null);
   const [previousWinner, setPreviousWinner] = useState(null);
+  const [playerName, setPlayerName] = useState("Player Name");
+  const submitName = (playerName) => {
+    setPlayerName(document.getElementById("nameBox").value)
+  }
   const onplay = (userChoice) => {
     let itemArray = Object.keys(choices)
     let randomNum = Math.floor(Math.random() * itemArray.length)
     let itemName = itemArray[randomNum]
 
-    judgment(choices[userChoice],choices[itemName])
-    
+    judgment(choices[userChoice], choices[itemName])
+
     setUserC(choices[userChoice])
     setComputerC(choices[itemName])
     gameHistory.push(result);
     setGameHistory(gameHistory)
   }
 
-  const judgment = (user,computer) => {
+  const judgment = (user, computer) => {
     if (user.name === "rock") {
       result = computer.name === "scissors" ? "Victory!" : "Defeat!";
     }
@@ -49,13 +55,21 @@ function App() {
     }
 
     if (user.name === computer.name) result = "Tie game!";
-    console.log(user.name, computer.name, result)
     if (result === "Victory!") {
       setPreviousWinner("You");
+      className = "victory";
     } else if (result === "Defeat!") {
       setPreviousWinner("Computer");
+      className = "defeat";
     } else {
       setPreviousWinner("Tie");
+      className = "tie";
+    }
+
+    if (gameHistory[gameHistory.length-1] === "Victory!" && gameHistory[gameHistory.length-2] === "Victory!" && gameHistory[gameHistory.length-3] === "Victory!") {
+     setFlawless("Flawless Victory!")
+    } else if (gameHistory[gameHistory.length-1] === "Defeat!" && gameHistory[gameHistory.length-2] === "Defeat!" && gameHistory[gameHistory.length-3] === "Defeat!") {
+      setFlawless("Flawless Victory!")
     }
 
   }
@@ -63,20 +77,27 @@ function App() {
     <div className="App">
       <ChoiceCard title="You" choice={userC} previousWinner={previousWinner} />
       <div className="format">
+        <div>
+          <h4>{flawless}</h4>
+        </div>
         <button className="btn btn-info btn-lg" onClick={() => onplay('rock')}>Rock</button>
         <button className="btn btn-info btn-lg" onClick={() => onplay('paper')}>Paper</button>
         <button className="btn btn-info btn-lg" onClick={() => onplay('scissors')}>Scissors</button>
         <div>
-          <div>{result}</div>
+          <h2 className={`${className}`}>{result}</h2>
         </div>
       </div>
       <ChoiceCard title="Computer" choice={computerC} previousWinner={previousWinner} />
-      <div className="col-md-2 themed-grid-col historyFormat">
-        <h3>History</h3>
-        <div>
-          {gameHistory.map(result => {
-            return <div>{result}</div>;
-          })}
+      <div className="col-md-3 themed-grid-col">
+        <input type="text" id="nameBox" placeholder="Enter name here..."></input>
+        <button className="btn btn-info" onClick={() => submitName()}>Submit</button>
+        <div class="historyFormat">
+          <h3>History</h3>
+          <div>
+            {gameHistory.map(result => {
+              return <div>{playerName}: {result}</div>;
+            })}
+          </div>
         </div>
       </div>
     </div>
